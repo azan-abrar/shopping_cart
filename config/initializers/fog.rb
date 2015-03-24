d27ff6d31a31aac08b1ddc915fead033f3e04d00
Azan Abrar <azan.abrar@gmail.com>
2015-03-24 17:14:59 +0500

    added more customizations for aws

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,6 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key}
-  config.fog_directory  = 'shopping_cart'
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+	host: Rails.application.secrets.aws_host_name}
+  config.fog_directory  = Rails.application.secrets.aws_directory
 end
